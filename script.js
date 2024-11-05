345214dcc4c4fa3ad60d755dcde051bc0fde44dc
bini-x <erblinhiseni@gmail.com>
2024-11-05 07:53:22 +0100

    new

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function addTask(task) {
   modifyButton.classList.add("modifyButton");
   modifyButton.textContent = "Modifiko";
   deleteButton.textContent = "Fshij";
+  const dueDate = document.createElement("p");
+  const date = new Date();
+  const datePrompt = prompt("Sheno daten e perfundimit(formati: YYYY-MM-DD): ");
+  date.setDate(datePrompt);
+  dueDate.textContent = `Data e Fundit: ${datePrompt}`;
+  dueDate.classList.add("dueDate");
   deleteButton.addEventListener("click", () => {
     taskList.removeChild(li);
   });
@@ -20,6 +26,7 @@ function addTask(task) {
     taskList.removeChild(li);
     addTask(editedTask);
   });
+  li.appendChild(dueDate);
   li.appendChild(buttonDiv);
   buttonDiv.appendChild(modifyButton);
   buttonDiv.appendChild(deleteButton);
@@ -30,4 +37,6 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addTask(taskInput.value);
   taskInput.value = "";
+
+  function setDate() {}
 });
